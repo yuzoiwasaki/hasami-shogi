6d11145104b0d23f37c71c6a56ce9d620399092d
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2025-03-23 23:03:37 +0900

    Fix room name

--- src/constants/rooms.ts
+++ src/constants/rooms.ts
@@ -3,11 +3,11 @@ export const SHOGI_ROOMS = [
   { id: 'takao', name: '高雄の間' },
   { id: 'kiho', name: '棋峰の間' },
   { id: 'unkaku', name: '雲鶴の間' },
-  { id: 'hiyen', name: '飛燕の間' },
-  { id: 'ginsha', name: '銀沙の間' },
-  { id: 'kei', name: '桂の間' },
+  { id: 'hien', name: '飛燕の間' },
+  { id: 'ginsa', name: '銀沙の間' },
+  { id: 'katsura', name: '桂の間' },
   { id: 'koun', name: '香雲の間' },
-  { id: 'hogetu', name: '歩月の間' },
+  { id: 'hozuki', name: '歩月の間' },
 ] as const;
 
 export type RoomId = typeof SHOGI_ROOMS[number]['id']; 
